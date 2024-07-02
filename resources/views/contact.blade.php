b6824f5608de50c32d9fba4f92a2573e3db9b4a9
Sanke0126 <nambaashdee@gmail.com>
2024-07-02 10:28:35 +0800

    Php

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:title>Contact</x-slot:title>
+    <h1>This is a contact page</h1>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:title>Home</x-slot:title>
+    <h1>This is the home page</h1>
+</x-layout>
