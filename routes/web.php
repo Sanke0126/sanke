e5cc16ac296535215b4726b0f0ec719073e2fa59
Sanke0126 <nambaashdee@gmail.com>
2024-07-03 09:55:08 +0800

    Bam Bam

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,18 @@ Route::get('/jobs', function () {
     ]);
 });
 
+//Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+//Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
     return view('jobs.show', ['job' => $job]);
 });
 
+//Store
 Route::post('/jobs', function () {
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -35,6 +38,34 @@ Route::post('/jobs', function () {
     ]);
     return redirect('/jobs');
 });
+
+//Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+    return view('jobs.edit', ['job' => $job]);
+});
+
+//Update
+Route::patch('/jobs/{id}', function ($id) {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+    $job = Job::find($id);
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+    return redirect('/jobs/' . $job->id);
+});
+
+// //Delete
+Route::delete('/jobs/{id}', function ($id) {
+    Job::find($id)->delete();
+    return redirect('/jobs');
+});
+
+
 Route::get('/contact', function () {
     return view('contact');
 });
