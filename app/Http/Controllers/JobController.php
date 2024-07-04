8603534babccdd4564b9fc4b38ffbe6ad7ba9d22
Sanke0126 <nambaashdee@gmail.com>
2024-07-04 12:51:38 +0800

    123

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\JobPosted;
 use App\Models\Job;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 
 class JobController extends Controller
 {
@@ -28,11 +30,12 @@ class JobController extends Controller
             'title' => ['required', 'min:3'],
             'salary' => ['required'],
         ]);
-        Job::create([
+        $job = Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1
         ]);
+        Mail::to($job->employer->user)->queue(new JobPosted($job));
         return redirect('/jobs');
     }
     public function edit(Job $job)
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
+
+
 Route::resource('jobs', JobController::class)->middleware('auth');
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
